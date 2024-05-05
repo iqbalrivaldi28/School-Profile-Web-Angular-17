5bd43a7a87dfdd259151093ccad1daa2df312a57
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-05 10:26:31 +0700

    feat: first upload

---
+++ src/app/template/portfolio-web/portfolio-web.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-web',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-web.component.html',
+  styleUrl: './portfolio-web.component.scss'
+})
+export class PortfolioWebComponent {
+
+}
