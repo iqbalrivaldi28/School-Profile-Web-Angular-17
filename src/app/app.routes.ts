73935294c62d2eb42805b8590545f227048cafab
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-05 11:12:57 +0700

    feat: add material angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PortfolioWebComponent } from './template/portfolio-web/portfolio-web.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PortfolioWebComponent,
+  },
+];
--- src/app/template/portfolio-web/portfolio-web.component.ts
+++ src/app/template/portfolio-web/portfolio-web.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-portfolio-web',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './portfolio-web.component.html',
-  styleUrl: './portfolio-web.component.scss'
+  styleUrl: './portfolio-web.component.scss',
 })
-export class PortfolioWebComponent {
-
-}
+export class PortfolioWebComponent {}
