9e330663b3006963faa39cba4a8396643367e73e
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-05 14:13:23 +0700

    feat: add component sambutan

--- src/app/template/portfolio-web/portfolio-web.component.ts
+++ src/app/template/portfolio-web/portfolio-web.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { HeaderMenuComponent } from '../../components/header-menu/header-menu.component';
+import { AboutUsComponent } from 'src/app/components/about-us/about-us.component';
 
 @Component({
   selector: 'app-portfolio-web',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, HeaderMenuComponent],
+  imports: [
+    HeaderComponent,
+    BannerComponent,
+    HeaderMenuComponent,
+    AboutUsComponent,
+  ],
   templateUrl: './portfolio-web.component.html',
   styleUrl: './portfolio-web.component.scss',
 })
